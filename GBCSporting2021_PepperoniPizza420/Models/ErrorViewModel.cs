8811ce07fee0b528b28a6f042f4ec09a43a9f27e
Simon Bermudez <sabdz55@gmail.com>
2021-02-05 20:43:14 -0500

    Initial Commit

---
+++ GBCSporting2021_PepperoniPizza420/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GBCSporting2021_PepperoniPizza420.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
